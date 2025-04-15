7dcd13f35ea9e9b90a5a17ff579fcbf26a975c8e
Baran Ates <baran.ates@accenture.com>
2025-04-16 00:27:43 +0300

    Initial Commit

---
+++ Football Chants Ukit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football Chants Ukit
+//
+//  Created by baran.ates on 16.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Football Chants UkitTests/Football_Chants_UkitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Football_Chants_UkitTests.swift
+//  Football Chants UkitTests
+//
+//  Created by baran.ates on 16.04.2025.
+//
+
+import Testing
+@testable import Football_Chants_Ukit
+
+struct Football_Chants_UkitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
